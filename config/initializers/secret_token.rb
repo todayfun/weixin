f309bbed46d213abedc06f9a4b68edf3a797dc88
boyi <boyi@taobao.com>
2014-12-20 16:27:37 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weixin::Application.config.secret_token = '64163d8374fe4df490dc5d65639471c130f7472c2edef59a6f4421e8ffff7c3c9598dbfbfadd47d4602b21934fe092a7c9a3b76bc768ea972bc21229d5107e47'
