92a5288dbd188b2502882ae3da1a8b0c5599415d
boyi <boyi@taobao.com>
2014-12-21 10:40:40 +0800

    add

--- app/controllers/kanjia_controller.rb
+++ app/controllers/kanjia_controller.rb
@@ -5,13 +5,13 @@ class KanjiaController < ApplicationController
   EncodingAESKey = "vcUad8cqlPN9fV7FWX0dRNrZ6svGf34yaGITiz5QGX8"
   
   def verify    
-    str = [TOKEN,params[:timestamp].to_s,params[:nonce].to_s].sort.join('')
+    str = [TOKEN,params[:timestamp],params[:nonce]].sort.join()
     sha1 = Digest::SHA1.hexdigest(str)
     
-    Rails.logger.info("str:#{str},sig:#{sha1}")
-    if sha1 == params[:signature].to_s
+    Rails.logger.info("str:#{str},sig:#{sha1}, signature:#{params[:signature]}")
+    if sha1 == params[:signature]
       respond_to do |format|
-        format.html {render :text=>parmas[:echostr]}
+        format.html {render :text=>params[:echostr]}
       end
     else
       respond_to do |format|
@@ -19,4 +19,5 @@ class KanjiaController < ApplicationController
       end
     end    
   end
+
 end
