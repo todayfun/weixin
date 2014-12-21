6fcc54b06c4b783cb6e14cf18f8f18e7afebad0d
boyi <boyi@taobao.com>
2014-12-21 10:17:50 +0800

    check weixin signature

--- app/controllers/kanjia_controller.rb
+++ app/controllers/kanjia_controller.rb
@@ -2,6 +2,7 @@ require 'digest/sha1'
 
 class KanjiaController < ApplicationController
   TOKEN = "450013807_kanjia"
+  EncodingAESKey = "vcUad8cqlPN9fV7FWX0dRNrZ6svGf34yaGITiz5QGX8"
   
   # signature	微信加密签名，signature结合了开发者填写的token参数和请求中的timestamp参数、nonce参数。
   # timestamp	时间戳
@@ -11,7 +12,9 @@ class KanjiaController < ApplicationController
   def verify    
     str = [TOKEN,params[:timestamp].to_s,params[:nonce].to_s].sort.join('')
     sha1 = Digest::SHA1.hexdigest(str)
-    if sha1 == params[:signature]
+    
+    Rails.logger.info("str:#{str},sig:#{sha1}")
+    if sha1 == params[:signature].to_s
       respond_to do |format|
         format.html {render :text=>parmas[:echostr]}
       end
