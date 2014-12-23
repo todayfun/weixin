b9c0eb226abcc8563ddd2b0f85e2fa865f1be29d
boyi <boyi@taobao.com>
2014-12-23 20:43:43 +0800

    fix

--- app/controllers/kanjia_controller.rb
+++ app/controllers/kanjia_controller.rb
@@ -24,7 +24,7 @@ class KanjiaController < ApplicationController
       to = params[:xml][:ToUserName]            
       Fans.subscribe_by(from) if "subscribe"==params[:xml][:Event]
       
-      msg = WeixinHelper.echo_game(from,to,url_for(:action=>:kanjia,:game=>Game.default.guid,:cmd=>"gameview"))
+      msg = WeixinHelper.echo_game(from,to,WeixinHelper.with_auth(url_for(:action=>:kanjia,:game=>Game.default.guid,:cmd=>"gameview")))
       
       respond_to do |format|
         format.html {render :text=>msg}
@@ -41,30 +41,37 @@ class KanjiaController < ApplicationController
     
     redirect_url = nil
     cmd = params[:cmd]
-    case cmd
-    # gameview 
-    # link:kanjia?game=guid&cmd=gameview
-    # click play: weixin_auth(kanjia?game=guid&cmd=gamelaunch)
-    # share: kanjia?game=guid&cmd=gameview
-    when "gameview"
-      game = Game.find_by_guid(params[:game])
-      if game        
-        @title = "原始价格"
-        @label = view_context.link_to("参与砍价",WeixinHelper.with_auth(url_for(:game=>game.guid,:cmd=>"gamelaunch")))
-        @share_url = url_for(:game=>game.guid,:cmd=>"gameview")
-        @links = [view_context.link_to("查看砍价规则",url_for(:action=>"rule",:game=>game.guid)),view_context.link_to("查看砍价排行",url_for(:action=>"topn",:game=>game.guid))]
-      else          
-        @label = "#{cmd} fail: cant found game"        
-      end
-      
-    # launchgame
-    # link: kanjia?game=guid&cmd=gamelaunch
-    # subscribe?：redirect_to weixin_auth(kanjia?play=guid&cmd=playview)
-    # unsubscribe?：redirect_to subscribe
-    when "gamelaunch"
-      openid = WeixinHelper.query_openid(params[:code])
-      
-      if openid
+    openid = WeixinHelper.query_openid(params[:code])
+    if openid.nil?
+      @label = "#{cmd} fail: cant get openid"
+    else
+      case cmd
+      # gameview 
+      # link:kanjia?game=guid&cmd=gameview
+      # click play: weixin_auth(kanjia?game=guid&cmd=gamelaunch)
+      # share: kanjia?game=guid&cmd=gameview
+      when "gameview"
+        game = Game.find_by_guid(params[:game])
+        if game        
+          play = Play.where(:game_guid=>game.guid,:owner=>openid).first
+          if play
+            @title = "已经创建了游戏，直接进入"
+            redirect_url = WeixinHelper.with_auth(url_for(:play=>play.guid,:cmd=>"playview"))
+          else
+            @title = "原始价格"
+            @label = view_context.link_to("参与砍价",WeixinHelper.with_auth(url_for(:game=>game.guid,:cmd=>"gamelaunch")))
+            @share_url = WeixinHelper.with_auth(url_for(:game=>game.guid,:cmd=>"gameview"))
+            @links = [view_context.link_to("查看砍价规则",url_for(:action=>"rule",:game=>game.guid)),view_context.link_to("查看砍价排行",url_for(:action=>"topn",:game=>game.guid))]
+          end          
+        else          
+          @label = "#{cmd} fail: cant found game"        
+        end
+
+      # launchgame
+      # link: kanjia?game=guid&cmd=gamelaunch
+      # subscribe?：redirect_to weixin_auth(kanjia?play=guid&cmd=playview)
+      # unsubscribe?：redirect_to subscribe
+      when "gamelaunch"
         fans = Fans.find_by_openid openid
         if fans
           game = Game.find_by_guid(params[:game])
@@ -77,7 +84,7 @@ class KanjiaController < ApplicationController
               play.guid = WeixinHelper.guid
               play.save
             end
-            
+
             @label = "game launched!"
             redirect_url = WeixinHelper.with_auth(url_for(:play=>play.guid,:cmd=>"playview"))            
           else
@@ -87,22 +94,17 @@ class KanjiaController < ApplicationController
           @label = "#{cmd} fail: 还没订阅公众号"
           redirect_url = url_for(:action=>"subscribe")          
         end
-      else
-        @label = "#{cmd} fail: cant get openid"
-      end
-      
-    # playview
-    # link: weixin_auth(kanjia?play=guid&cmd=playview)
-    # doplay: weixin_auth(kanjia?play=guid&cmd=doplay)
-    # share: weixin_auth(kanjia?play=guid&cmd=playview)  
-    when "playview"
-      openid = WeixinHelper.query_openid(params[:code])
-      if openid
+
+      # playview
+      # link: weixin_auth(kanjia?play=guid&cmd=playview)
+      # doplay: weixin_auth(kanjia?play=guid&cmd=doplay)
+      # share: weixin_auth(kanjia?play=guid&cmd=playview)  
+      when "playview"
         play = Play.find_by_guid params[:play]
         if play          
           @title = "当前战绩"
-          @share_url = WeixinHelper.with_auth(url_for(:action=>"kanjia",:play=>play.guid,:cmd=>"playview"))
-          
+          @share_url = WeixinHelper.with_auth(url_for(:play=>play.guid,:cmd=>"playview"))
+
           owner = play.owner
           if play.friends.include?(openid)            
             @label = "您已经砍过了，明天再来"
@@ -121,17 +123,12 @@ class KanjiaController < ApplicationController
         else          
           @label = "#{cmd} fail: cant found play"
         end
-      else
-        @label = "#{cmd} fail: cant get openid"
-      end
-    
-    # playview
-    # link: weixin_auth(kanjia?play=guid&cmd=playview)
-    # doplay: weixin_auth(kanjia?play=guid&cmd=doplay)
-    # share: weixin_auth(kanjia?play=guid&cmd=playview)    
-    when "doplay"
-      openid = WeixinHelper.query_openid(params[:code])
-      if openid
+
+      # playview
+      # link: weixin_auth(kanjia?play=guid&cmd=playview)
+      # doplay: weixin_auth(kanjia?play=guid&cmd=doplay)
+      # share: weixin_auth(kanjia?play=guid&cmd=playview)    
+      when "doplay"
         play = Play.find_by_guid(params[:play])
         if play
           @label = "do play"
@@ -140,11 +137,10 @@ class KanjiaController < ApplicationController
           @label = "#{cmd} fail: cant found play"
         end
       else
-        @label = "#{cmd} fail: cant get openid"
+        @label = "invalid cmd"
       end
-    else
-      @label = "invalid cmd"
-    end        
+    end
+           
     
     if redirect_url
       respond_to do |format|
